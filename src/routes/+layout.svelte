955b5f65bf867dbcae0ed4d7f858e192097fb991
Ewe <181084618+DieEwe@users.noreply.github.com>
2025-02-12 14:23:34 +0100

    appicon als png

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 	  <!-- Left side: brand/logo -->
 	  <a href="/" class="flex items-center">
 		 <!-- If you have a logo, place an <img> here: -->
-		 <img src="/favicon.svg" alt="Inklu-Solutions Logo" class="w-8 h-8 md:w-11 md:h-11 lg:w-14 lg:h-14" />
+		 <img src="/AppIcon.png" alt="Inklu-Solutions Logo" class="w-8 h-8 md:w-11 md:h-11 lg:w-14 lg:h-14" />
 		 <!-- If you have a text place it here: -->
 		<!--<span class="text-xl font-bold tracking-tight text-gray-900">My Web App</span>-->
 	  </a>
